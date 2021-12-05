ea41d49379a12d616c90c6fa4f741663e211e8c4
kostyabek <kostyabek@gmail.com>
2021-12-05 11:36:40 +0200

    Update.

--- CourseWork.Infrastructure/Queries/Thread/GetPopularThreads/GetPopularThreadsQueryHandler.cs
+++ CourseWork.Infrastructure/Queries/Thread/GetPopularThreads/GetPopularThreadsQueryHandler.cs
@@ -4,7 +4,6 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using CourseWork.Core.Database;
-using CourseWork.Core.Database.DatabaseConnectionHelper;
 using CourseWork.Core.Database.Entities.Boards;
 using CourseWork.Core.Database.Entities.Files;
 using CourseWork.Core.Database.Entities.Replies;
@@ -23,6 +22,7 @@ using Microsoft.Extensions.Logging;
 namespace CourseWork.Core.Queries.Thread.GetPopularThreads
 {
     using Common.Consts;
+    using Helpers.DatabaseConnectionHelper;
 
     /// <summary>
     /// GetPopularThreadsQuery handler.
